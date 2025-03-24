776daa416d1c662a3c347c13898516021e74604a
Surya221299 <60531747+Surya221299@users.noreply.github.com>
2025-03-24 22:50:38 +0700

    Add files via upload

---
+++ FirstChallangeSurya/FirstChallangeSuryaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstChallangeSuryaApp.swift
+//  FirstChallangeSurya
+//
+//  Created by Surya on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstChallangeSuryaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
